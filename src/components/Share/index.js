a3fc16500582ab820650e9ff7df89adaf0c43c46
mohwa <yanione2@gmail.com>
2019-06-26 18:20:34 +0900

    Add isSupportedNavigatorShare method

--- src/components/Share/index.js
+++ src/components/Share/index.js
@@ -7,9 +7,11 @@ import './Share.scss'
 import config from '../../../config'
 
 export default class Share extends Component {
-  isSupportedNavigatorShare = false
-  componentDidMount () {
-    this.isSupportedNavigatorShare = navigator.share ? true : false
+  isSupportedNavigatorShare () {
+    if (typeof window === 'undefined') {
+      return false
+    }
+    return navigator.share ? true : false
   }
   openShareModel (text, url) {
     navigator.share({
@@ -25,8 +27,7 @@ export default class Share extends Component {
     const url = config.siteUrl + realPrefix + slug
     let shareHtml
 
-
-    if (this.isSupportedNavigatorShare) {
+    if (this.isSupportedNavigatorShare()) {
       shareHtml =
         <div className='Share flex justify-end pa3'>
 	        <a className='share-icon' onClick={() => this.openShareModel(title, url)} />
@@ -40,7 +41,7 @@ export default class Share extends Component {
                 <svg data-icon='twitter' viewBox='0 0 32 32' style={{ fill: 'currentcolor' }}>
                   <title>twitter icon</title>
                   <path
-                    d='M2 4 C6 8 10 12 15 11 A6 6 0 0 1 22 4 A6 6 0 0 1 26 6 A8 8 0 0 0 31 4 A8 8 0 0 1 28 8 A8 8 0 0 0 32 7 A8 8 0 0 1 28 11 A18 18 0 0 1 10 30 A18 18 0 0 1 0 27 A12 12 0 0 0 8 24 A8 8 0 0 1 3 20 A8 8 0 0 0 6 19.5 A8 8 0 0 1 0 12 A8 8 0 0 0 3 13 A8 8 0 0 1 2 4' />
+                    d='M2 4 C6 8 10 12 15 11 A6 6 0 0 1 22 4 A6 6 0 0 1 26 6 A8 8 0 0 0 31 4t A8 8 0 0 1 28 8 A8 8 0 0 0 32 7 A8 8 0 0 1 28 11 A18 18 0 0 1 10 30 A18 18 0 0 1 0 27 A12 12 0 0 0 8 24 A8 8 0 0 1 3 20 A8 8 0 0 0 6 19.5 A8 8 0 0 1 0 12 A8 8 0 0 0 3 13 A8 8 0 0 1 2 4' />
                 </svg>
               </span>
             </TwitterShareButton>
