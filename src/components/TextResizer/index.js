ff2af50a0b8cdbc79153d7d64d5750a1446085a1
mohwa <yanione2@gmail.com>
2019-06-27 14:41:54 +0900

    Add refresh function for text size

--- src/components/TextResizer/index.js
+++ src/components/TextResizer/index.js
@@ -22,8 +22,25 @@ const decrementFontSize = (elemList) => {
 	})
 }
 
+const getOriginalFontSize = (elemList) => {
+	return elemList.reduce((acc, elem) => {
+		const oldStyle = getComputedStyle(elem)
+
+		acc.push(parseInt(oldStyle.fontSize))
+
+		return acc
+	}, [])
+}
+
+const resetFontSize = (elemList, originalFontSizeList) => {
+	elemList.forEach((elem, index) => {
+		elem.style.fontSize = `${parseInt(originalFontSizeList[index])}px`
+	})
+}
+
 export default class TextResizer extends React.Component {
 	elemList = null
+	originalFontSizeList = null
 	componentDidMount () {
 		const title = document.querySelector('header h1.f3.f2-m.f-subheadline-l.measure.lh-title.fw1.mt0')
 		const date = document.querySelector('div time.f5.f4-l.db.fw1.baskerville.mb4-l.mb2')
@@ -31,6 +48,7 @@ export default class TextResizer extends React.Component {
 		const content = document.querySelector('section div div.measure.db.center.f5.f4-ns.lh-copy')
 
 		this.elemList = [title, date, content].concat(...tags)
+		this.originalFontSizeList = getOriginalFontSize(this.elemList)
 	}
 
 	render () {
@@ -40,8 +58,9 @@ export default class TextResizer extends React.Component {
 
 		return (
 			<div className='TextResizer flex justify-center'>
-				<div className='more mr1' onClick={ () => { incrementFontSize(this.elemList) }} />
-				<div className='substract ml1' onClick={ () => { decrementFontSize(this.elemList) }} />
+				<div className='more' onClick={ () => { incrementFontSize(this.elemList) }} />
+				<div className='substract ml2' onClick={ () => { decrementFontSize(this.elemList) }} />
+				<div className='refresh ml2' onClick={ () => { resetFontSize(this.elemList, this.originalFontSizeList) }} />
 			</div>
 		)
 	}
