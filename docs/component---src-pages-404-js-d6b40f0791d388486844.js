563e5f7eb13273b0114362065052c9d8d8b6ae22
mohwa <yanione2@gmail.com>
2022-01-09 09:48:50 +0900

    Add docs

---
+++ docs/component---src-pages-404-js-d6b40f0791d388486844.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmohwa_github_io=self.webpackChunkmohwa_github_io||[]).push([[883],{9616:function(t,e,a){a.r(e);var n=a(7294),o=a(7198),u=a(3751);e.default=function(t){var e=t.data,a=t.location,i=e.site.siteMetadata.title;return n.createElement(o.Z,{location:a,title:i},n.createElement(u.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-d6b40f0791d388486844.js.map
