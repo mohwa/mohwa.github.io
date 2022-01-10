7c5891937b06ddce23ada2e5bbf4e1d8718d4c8e
mohwa <yanione2@gmail.com>
2022-01-10 13:08:27 +0900

    new posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,8 @@ module.exports = {
           {
             resolve: 'gatsby-remark-code-buttons',
             options: {
-              tooltipText: `Click to copy`,
+              // tooltipText: `Click to copy`,
+              toasterText: 'Copied!',
               svgIcon: `<?xml version="1.0" encoding="utf-8"?>
 <!-- Generator: Adobe Illustrator 19.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
